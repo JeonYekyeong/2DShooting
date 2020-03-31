6eb923d5fb1d4e047b2606da62f340b976331e99
YKJ <jeonyk219@gmail.com>
2020-03-31 23:04:46 +0900

    Frame
    필요한 리소스 배치

---
+++ Shooting2D/Assets/Script/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // 좌우로 움직이기
+    // 행성과 일정횟수만큼 충돌할 경우 GameOver
+    // 코인 획득하면 피버타임
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
