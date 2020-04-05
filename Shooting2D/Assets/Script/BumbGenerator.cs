0c1283bbb9ff3580358894db550a636d9751fd7b
YKJ <jeonyk219@gmail.com>
2020-04-05 13:32:57 +0900

    Bumb shooting
    폭탄 던지기

---
+++ Shooting2D/Assets/Script/BumbGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumbGenerator : MonoBehaviour
+{
+    // prefab
+    public GameObject bumb;
+
+    // player 위치
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 playerPos = this.player.transform.position;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bObj = Instantiate(bumb) as GameObject;
+            bObj.transform.position = new Vector2(playerPos.x - 1.5f, playerPos.y);
+        }
+    }
+}
--- Shooting2D/Assets/Script/Shooting.cs
+++ Shooting2D/Assets/Script/Shooting.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
+    Rigidbody2D b_rb;
+
+    // 폭탄 움직이는 속도
+    public float speed = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.b_rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        b_rb.velocity = new Vector2(1.0f,0) * -speed;
+
+        // 화면 밖에 나가면 삭제  -4.47 ~ 4.42
+        if (transform.position.x < -10.94 || transform.position.y <= -5.28 || transform.position.y >= 5.3)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // 행성과 충돌하면 삭제
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
     }
 }
